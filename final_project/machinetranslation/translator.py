6521241f4892285c72259d86596060f00a2e1f17
robin-pouget <robin.pouget@orange.fr>
2021-08-26 16:11:48 +0000

    mise à jour du repo

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -19,17 +19,22 @@ language_translator.set_service_url(url)
 
 def english_to_french(english_text, l_t):
     """ traduit anglais en francais"""
-
-    translation = l_t.translate(
-        text=english_text,
-        model_id='en-fr').get_result()
-    french_text = translation['translations'][0]['translation']
+    if english_text is None:
+        french_text = None
+    else:
+        translation = l_t.translate(
+            text=english_text,
+            model_id='en-fr').get_result()
+        french_text = translation['translations'][0]['translation']
     return french_text
 
 def french_to_english(french_text,l_t):
     """ traduit francais en anglais"""
-    translation = l_t.translate(
-        text=french_text,
-        model_id='fr-en').get_result()
-    english_text =  translation['translations'][0]['translation']
+    if french_text is None:
+        english_text=None
+    else:
+        translation = l_t.translate(
+            text=french_text,
+            model_id='fr-en').get_result()
+        english_text =  translation['translations'][0]['translation']
     return english_text
