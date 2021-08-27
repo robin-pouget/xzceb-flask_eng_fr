bc18bbefc815a15fed4afec2470ae5efb244c6b0
robin-pouget <robin.pouget@orange.fr>
2021-08-27 07:44:47 +0000

    maj tests

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -8,12 +8,12 @@ class testTranslator(unittest.TestCase):
     def test_english_to_FrenchOK(self):
         self.assertEqual(english_to_french('Hello'),'Bonjour')
     
-    def test_french_to_englishOK(self):
-        self.assertEqual(french_to_english('bonjour'),'Hello')
-
     def test_english_to_FrenchNOK(self):
         self.assertNotEqual(english_to_french('Bye'),'Bonjour')
-    
+
+    def test_french_to_englishOK(self):
+        self.assertEqual(french_to_english('bonjour'),'Hello')
+   
     def test_french_to_englishNOK(self):
         self.assertNotEqual(french_to_english('Au revoir'),'Hello')
 
